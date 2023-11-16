70849e524c33fa25bacbf7aec60be6d36a85d1f4
BlackandBlue1908 <vincebalsamo@gmail.com>
2023-11-16 15:09:17 -0500

    adds new line but doesnt delete old.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 def process_labels(labels):
     relevant_labels = {}
     if isinstance(labels, dict):
@@ -60,40 +61,55 @@ def read_intermediary_file(intermediary_path):
 
 def update_output_file(output_path, intermediary_data):
     try:
-        # Create a mapping of container to 'hostip dns' pair
-        hostip_dns_mapping = {container: f"{labels.get('pihole.hostip', 'unknown')} {labels.get('pihole.dns', 'unknown')}" 
-                              for container, labels in intermediary_data.items()}
+        # Create a mapping from existing hostip-dns pairs to container names
+        existing_pairs_to_container = {f"{labels.get('pihole.hostip', 'unknown')} {labels.get('pihole.dns', 'unknown')}": container
+                                       for container, labels in intermediary_data.items()}
 
-        # Read the existing file, update if the container exists, otherwise keep as is
-        updated_content = []
-        existing_containers = set()
+        # Read existing file and prepare to update lines
         with open(output_path, 'r') as file:
-            for line in file:
-                container = line.split()[0]
-                if container in hostip_dns_mapping:
-                    updated_content.append(f"{container} {hostip_dns_mapping[container]}\n")
-                    existing_containers.add(container)
-                else:
-                    updated_content.append(line)
+            existing_lines = file.readlines()
 
-        # Add new entries for containers not already in the file
-        for container, hostip_dns in hostip_dns_mapping.items():
-            if container not in existing_containers:
-                updated_content.append(f"{container} {hostip_dns}\n")
+        # Create a set for updated content and a set to track which containers have been updated
+        updated_content = set()
+        updated_containers = set()
 
-        # Write the updated content to the file
+        # Update existing lines with new hostip-dns values
+        for line in existing_lines:
+            line_clean = line.strip()
+            if line_clean in existing_pairs_to_container:
+                container = existing_pairs_to_container[line_clean]
+                new_pair = f"{intermediary_data[container].get('pihole.hostip', 'unknown')} {intermediary_data[container].get('pihole.dns', 'unknown')}"
+                updated_content.add(new_pair)
+                updated_containers.add(container)
+            else:
+                updated_content.add(line_clean)
+
+        # Add new hostip-dns pairs for containers not in the updated list
+        for container, labels in intermediary_data.items():
+            if container not in updated_containers:
+                new_pair = f"{labels.get('pihole.hostip', 'unknown')} {labels.get('pihole.dns', 'unknown')}"
+                updated_content.add(new_pair)
+
+        # Write updated content
         with open(output_path, 'w') as file:
-            file.writelines(updated_content)
+            for line in sorted(updated_content):
+                file.write(f"{line}\n")
 
         logging.info(f"Successfully updated {output_path}")
     except FileNotFoundError:
-        # If the file doesn't exist, create it and write all hostip-dns pairs
+        # Create file if it doesn't exist and write new pairs
         with open(output_path, 'w') as file:
-            for hostip_dns in hostip_dns_mapping.values():
-                file.write(f"{hostip_dns}\n")
+            for container, labels in intermediary_data.items():
+                pair = f"{labels.get('pihole.hostip', 'unknown')} {labels.get('pihole.dns', 'unknown')}"
+                file.write(f"{pair}\n")
         logging.info(f"Created and updated {output_path}")
     except Exception as e:
         logging.error(f"Error updating file: {e}")
+
+# ... rest of the script remains the same
+
+
+
 
 def main():
     compose_file = '/compose/docker-compose.yml'
