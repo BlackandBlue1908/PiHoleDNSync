d7daff44cc662682a39ca40d36e49a32f1de1800
BlackandBlue1908 <vincebalsamo@gmail.com>
2023-11-16 14:35:42 -0500

    working updates but container name included.

--- main.py
+++ main.py
@@ -60,31 +60,37 @@ def read_intermediary_file(intermediary_path):
 
 def update_output_file(output_path, intermediary_data):
     try:
-        # Create a flat list of all labels
-        all_labels = [(container, key, value) for container, labels in intermediary_data.items() for key, value in labels.items()]
+        # Create a mapping of container to 'hostip dns' pair
+        hostip_dns_mapping = {container: f"{labels.get('pihole.hostip', 'unknown')} {labels.get('pihole.dns', 'unknown')}" 
+                              for container, labels in intermediary_data.items()}
 
-        # Read existing file, keep lines that are not in the intermediary data
+        # Read the existing file, update if the container exists, otherwise keep as is
         updated_content = []
+        existing_containers = set()
         with open(output_path, 'r') as file:
-            existing_lines = file.readlines()
+            for line in file:
+                container = line.split()[0]
+                if container in hostip_dns_mapping:
+                    updated_content.append(f"{container} {hostip_dns_mapping[container]}\n")
+                    existing_containers.add(container)
+                else:
+                    updated_content.append(line)
 
-        for line in existing_lines:
-            if not any(f"{container} {key}: {value}" in line for container, key, value in all_labels):
-                updated_content.append(line)
+        # Add new entries for containers not already in the file
+        for container, hostip_dns in hostip_dns_mapping.items():
+            if container not in existing_containers:
+                updated_content.append(f"{container} {hostip_dns}\n")
 
-        # Add new label lines
-        for container, key, value in all_labels:
-            updated_content.append(f"{container} {key}: {value}\n")
-
-        # Write updated content
+        # Write the updated content to the file
         with open(output_path, 'w') as file:
             file.writelines(updated_content)
 
         logging.info(f"Successfully updated {output_path}")
     except FileNotFoundError:
-        with open(output_path, 'w') as file:  # Create file if it doesn't exist
-            for container, key, value in all_labels:
-                file.write(f"{container} {key}: {value}\n")
+        # If the file doesn't exist, create it and write all hostip-dns pairs
+        with open(output_path, 'w') as file:
+            for hostip_dns in hostip_dns_mapping.values():
+                file.write(f"{hostip_dns}\n")
         logging.info(f"Created and updated {output_path}")
     except Exception as e:
         logging.error(f"Error updating file: {e}")
